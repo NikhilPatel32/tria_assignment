791b7ce7528e20836117854f13bb703339a88ccb
NikhilPatel <leakee134@gmail.com>
2025-10-23 16:46:04 +0530

    add project initial setup and tailwind css setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='bg-black flex justify-center items-center text-white min-h-screen'>
+      Hello, I am building new project.
+    </div>
+  )
+}
+
+export default App
